c0017b635af4bb3408d312da7ac738a58d634ef2
aKirpichenok <a.kirpichenok99@gmail.com>
2023-03-29 15:30:05 +0300

    clear project files, made future architecture

---
+++ libs/ui/portal/Portal.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement } from 'react';
+import ReactDOM from 'react-dom';
+import cn from 'classnames';
+
+import styles from './Portal.module.scss';
+
+interface Props {
+	children: ReactElement;
+	display?: boolean;
+	zIndex?: number;
+	transparent?: boolean;
+	onWrapperClick?: () => void;
+}
+
+const Portal: React.FC<Props> = ({
+	children,
+	display = false,
+	transparent = false,
+	zIndex = 999,
+	onWrapperClick = () => undefined,
+}) => {
+	const [rootNode] = React.useState(document.getElementById('portal'));
+
+	if (rootNode === null || !display) {
+		return null;
+	}
+
+	const handleWrapperClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+		if (e.target === e.currentTarget) {
+			onWrapperClick();
+		}
+	};
+
+	return ReactDOM.createPortal(
+		<div
+			tabIndex={0}
+			role="button"
+			onClick={handleWrapperClick}
+			className={cn(styles.container, transparent && styles.transparent)}
+			style={{ zIndex }}
+		>
+			{display && children}
+		</div>,
+		rootNode
+	);
+};
+
+export default Portal;
---
+++ packages/routes/AppRouter.tsx
@@ -0,0 +1,44 @@
+import React, { lazy } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import Main from '~/../packages/src/app/components/Layouts/Main';
+import LazyRoute from '~/../packages/routes/LazyRoute';
+
+const InstallScreen = lazy(
+	() => import('~/../packages/src/app/webpages/Install/Install')
+);
+const ParticipateScreen = lazy(
+	() => import('../src/app/webpages/Participate/Participate')
+);
+
+const AppRoutes: React.FunctionComponent = () => (
+	<React.Suspense fallback={null}>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />}>
+					<Route
+						path="install"
+						element={
+							<LazyRoute>
+								<InstallScreen />
+							</LazyRoute>
+						}
+					/>
+					<Route
+						path="participate"
+						element={
+							<LazyRoute>
+								<ParticipateScreen />
+							</LazyRoute>
+						}
+					/>
+					<Route path="*" element={<Navigate to="/install" />} />
+				</Route>
+
+				<Route index element={<Navigate to="/install" />} />
+			</Routes>
+		</BrowserRouter>
+	</React.Suspense>
+);
+
+export default AppRoutes;
