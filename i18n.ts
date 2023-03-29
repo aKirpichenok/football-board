b7a1c9dfe0c19758fdd322c292528544a721d6d6
aKirpichenok <a.kirpichenok99@gmail.com>
2023-03-29 15:48:49 +0300

    check ci build

--- i18n.ts
+++ i18n.ts
@@ -5,21 +5,21 @@ import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   .init({
     // ns: ['content'],
     // whitelist: ['en', 'ru'],
     interpolation: { escapeValue: false },
     debug: process.env.APP_STAND !== 'prod',
-    fallbackLng: 'en',
+    fallbackLng: 'ru',
     defaultNS: 'content',
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
+    detection: {
+      order: ['queryString', 'cookie'],
+    }
   });
 
 export default i18n;
